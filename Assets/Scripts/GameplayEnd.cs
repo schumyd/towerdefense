3d84d895048eb6d948dacfec266f588f759ee787
schumyd <schumy.daraqutni21@gmail.com>
2020-11-24 19:15:51 +0700

    s

---
+++ Assets/Scripts/GameplayEnd.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameplayEnd : MonoBehaviour
+{
+	float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Data.isGameOver)
+		{
+			timer += Time.deltaTime;
+			if (timer > 2)
+			{
+				SceneManager.LoadScene("GameOver");
+			}
+			if (Input.GetKeyDown (KeyCode.Escape))
+			{
+				Application.Quit();
+			}
+		}
+    }
+}
